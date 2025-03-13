6e966d984a04d8fc001b464df2b1ff31148135ca
Mischa Koischwitz <mischa.koischwitz@akquinet.de>
2025-03-13 22:07:40 +0100

    Revert "move tab-bar into its own repository"
    This reverts commit 83bcc28561693781c9595d83fd5d1ae5939338f2.

---
+++ src/app/tab-bar/tab-bar.component.ts
@@ -0,0 +1,67 @@
+import { Component, computed, ElementRef, viewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { filter, fromEvent, map, merge, of, scan, startWith, switchMap } from 'rxjs';
+
+@Component({
+  selector: 'app-tab-bar',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './tab-bar.component.html',
+  styleUrl: './tab-bar.component.scss'
+})
+export class TabBarComponent {
+
+  readonly newTabButton = viewChild<ElementRef<HTMLButtonElement>>('newTabButton');
+
+  readonly newTabButtonAvailable = toObservable(this.newTabButton).pipe(
+    filter(elementRef => !!elementRef),
+    map(elementRef => elementRef.nativeElement)
+  );
+
+  readonly addNewTabEvent = this.newTabButtonAvailable.pipe(
+    switchMap(element => fromEvent(element, 'click')),
+    map(() => (tabs: string[]) => [...tabs, 'new tab'])
+  );
+
+  readonly updateEvents = merge(this.addNewTabEvent);
+
+  readonly tabs = toSignal(
+    this.httpGetTabs().pipe(
+      switchMap(initialTabs =>
+        this.updateEvents.pipe(
+          startWith(() => initialTabs),
+          scan(
+            (tabs, action) => action(tabs),
+            initialTabs
+          ),
+        )
+      )
+    ), {initialValue: []}
+  );
+
+
+  readonly windowWidth = toSignal(
+    fromEvent(window, 'resize').pipe(
+      map(() => window.innerWidth)
+    ),
+    {initialValue: window.innerWidth}
+  );
+
+  readonly isMobile = computed(() => this.windowWidth() <= 480);
+
+  protected readonly tabWidth = computed(() => {
+    const responsiveWidth = this.windowWidth() / this.tabs().length;
+    if (responsiveWidth < 80) return 44;
+    if (responsiveWidth > 280) return 280;
+    return responsiveWidth;
+  });
+
+  readonly visibleTabsLimit = computed(() =>
+    Math.floor(this.windowWidth() / this.tabWidth())
+  );
+
+  httpGetTabs() {
+    return of(['tab1', 'tab2', 'reallyLongTabName', 'tab3', 'tab4', 'reallyLongTabNameThatIsEvenLonger']);
+  }
+}
